6008e2b238fbb3f058b6a6f0aa90049fa7529e21
RobertUnfried <8124@htl.rennweg.at>
2021-11-19 17:19:16 +0100

    Fixed negative bug

--- D2/Bruch/Fraction.py
+++ D2/Bruch/Fraction.py
@@ -8,26 +8,31 @@ class Fraction:
     def __init__(self, zaehler: int = 1, nenner: int = 1):
         if nenner == 0:
             raise ArithmeticError
-        mult = 1
-        if nenner < 0:
-            mult = -1
-        self.zaehler = zaehler * mult
-        self.nenner = nenner * mult
+        self.positive = True
+        if nenner * zaehler < 0:
+            self.positive = False
+        self.zaehler = abs(zaehler)
+        self.nenner = abs(nenner)
 
     def __str__(self):
         num = ""
+        if not self.positive:
+            num = "-"
         frac = ""
         if self.zaehler // self.nenner != 0:
-            num = str(self.zaehler // self.nenner) + " "
+            num += str(self.zaehler // self.nenner) + " "
         if self.zaehler % self.nenner != 0:
             frac = str(self.zaehler % self.nenner) + "/" + str(self.nenner)
+        
+
         return num + frac
 
     def __repr__(self):
-        return self.__class__.__name__ + f"({self.zaehler}, {self.nenner})"
+        mult = 1 if self.positive else -1
+        return self.__class__.__name__ + f"({self.zaehler*mult}, {self.nenner})"
 
 
 if __name__ == '__main__':
     f = Fraction(5, 3)
     print(f)
-    print(repr(f))
+    print(repr(f))#
