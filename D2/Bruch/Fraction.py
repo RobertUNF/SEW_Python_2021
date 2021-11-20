177fc3a762fd146c7d84b0ef2bdcd2f02642942c
Robert Unfried <8124@htl.rennweg.at>
2021-11-20 10:50:15 +0100

    added numerator property

--- D2/Bruch/Fraction.py
+++ D2/Bruch/Fraction.py
@@ -10,70 +10,79 @@ class Fraction:
     def __init__(self, zaehler: int = 1, nenner: int = 1):
         if nenner == 0:
             raise ArithmeticError
-        self.positive = True
+        self._positive = True
         if nenner * zaehler < 0:
-            self.positive = False
-        self.zaehler = abs(zaehler)
-        self.nenner = abs(nenner)
+            self._positive = False
+        self._numerator = abs(zaehler)
+        self._donominator = abs(nenner)
         self.__reduce()
 
     def __str__(self):
         num = ""
-        if not self.positive:
+        if not self._positive:
             num = "-"
         frac = ""
-        if self.zaehler // self.nenner != 0:
-            num += str(self.zaehler // self.nenner) + " "
-        if self.zaehler % self.nenner != 0:
-            frac = str(self.zaehler % self.nenner) + "/" + str(self.nenner)
+        if self._numerator // self._donominator != 0:
+            num += str(self._numerator // self._donominator) + " "
+        if self._numerator % self._donominator != 0:
+            frac = str(self._numerator % self._donominator) + "/" + str(self._donominator)
         
 
         return num + frac
 
     def __repr__(self):
-        mult = 1 if self.positive else -1
-        return self.__class__.__name__ + f"({self.zaehler*mult}, {self.nenner})"
+        mult = 1 if self._positive else -1
+        return self.__class__.__name__ + f"({self._numerator * mult}, {self._donominator})"
 
     def __reduce(self):
         """
         Verwendeter Algorithmus: https://de.wikipedia.org/wiki/Euklidischer_Algorithmus (klassisch)
         """
-        x, y = self.zaehler, self.nenner
+        x, y = self._numerator, self._donominator
         while x!=y: x, y = min(x, y), abs(x-y)
-        self.zaehler, self.nenner = self.zaehler//y, self.nenner//y
+        self._numerator, self._donominator = self._numerator // y, self._donominator // y
     
     def __add__(self, other: "Fraction"):
-        return Fraction((self.zaehler*other.nenner)+(other.zaehler*self.nenner), self.nenner * other.nenner)
+        return Fraction((self._numerator * other._donominator) + (other._numerator * self._donominator), self._donominator * other._donominator)
     
     def __sub__(self, other: "Fraction"):
-        return Fraction((self.zaehler*other.nenner)-(other.zaehler*self.nenner), self.nenner * other.nenner)
+        return Fraction((self._numerator * other._donominator) - (other._numerator * self._donominator), self._donominator * other._donominator)
 
     def __mul__(self, other: "Fraction"):
-        return Fraction(self.zaehler*other.zaehler, self.nenner * other.nenner)
+        return Fraction(self._numerator * other._numerator, self._donominator * other._donominator)
 
     def __truediv__(self, other: "Fraction"):
-        return Fraction(self.zaehler*other.nenner, self.nenner * other.zaehler)
+        return Fraction(self._numerator * other._donominator, self._donominator * other._numerator)
 
     def __floordiv__(self, other: "Fraction"):
-        return Fraction((self.zaehler*other.nenner) // (self.nenner*other.zaehler), 1)
+        return Fraction((self._numerator * other._donominator) // (self._donominator * other._numerator), 1)
 
     def __eq__(self, other: "Fraction"):
-        return self.zaehler == other.zaehler and self.nenner == other.nenner and \
-            self.positive == other.positive
+        return self._numerator == other._numerator and self._donominator == other._donominator and \
+               self._positive == other._positive
 
     def __lt__(self, other: "Fraction"):
-        if self.positive != other.positive:
-            if self.positive:
+        if self._positive != other._positive:
+            if self._positive:
                 return True
             return False
-        return self.zaehler*other.nenner > other.zaehler*self.nenner
+        return self._numerator * other._donominator > other._numerator * self._donominator
 
     def __abs__(self, other: "Fraction"):
-        return Fraction(self.zaehler, self.nenner)
+        return Fraction(self._numerator, self._donominator)
 
-    
+    @property
+    def numerator(self):
+        """The numerator of the fraction"""
+        return self._numerator
 
-    
+    @numerator.setter
+    def numerator(self, value):
+        self._numerator = value
+
+    @numerator.deleter
+    def numerator(self):
+        del self._numerator
 
 
 if __name__ == '__main__':
